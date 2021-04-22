845bdf1a54f4b78c7e9e95facf4fc664d241f5ad
Vaidotas <vaidotas@gedzius.lt>
2021-04-22 19:20:45 +0700

    Extracted disk mesh stuff into separate component

--- Assets/Things/Disk/Disk.cs
+++ Assets/Things/Disk/Disk.cs
@@ -1,30 +1,22 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace HanoiTowers
 {
-    [RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer), typeof(DiskMesh))]
     public class Disk : MonoBehaviour
     {
-        [SerializeField] float minRadius = 0.5f;
-        [SerializeField] float maxRadius = 1.2f;
-        [SerializeField] float innerRadius = 0.3f;
-        [SerializeField] int numberOfSegments = 32;
-        [SerializeField] float height = 0.3f;
         [SerializeField] Material defaultMaterial;
         [SerializeField] Material highlightMaterial;
-
-        MeshFilter meshFilter;
+        
         MeshRenderer meshRenderer;
-        List<Vector3> vertices;
-        List<int> triangles;
+        DiskMesh diskMesh;
         bool highlight;
         bool visible;
         int size;
 
         public int MaxDisks { get; set; }
-        public float Height => height;
-        
+        public float Height => diskMesh.Height;
+
         public int Size
         {
             get => size;
@@ -35,11 +27,11 @@ namespace HanoiTowers
 
                 if (oldSize != size)
                 {
-                    BuildMesh();
+                    diskMesh.Build(size, MaxDisks);
                 }
             }
         }
-        
+
         public bool Visible
         {
             get => visible;
@@ -62,82 +54,14 @@ namespace HanoiTowers
 
         void Awake()
         {
-            meshFilter = GetComponent<MeshFilter>();
             meshRenderer = GetComponent<MeshRenderer>();
-            vertices = new List<Vector3>();
-            triangles = new List<int>();
+            diskMesh = GetComponent<DiskMesh>();
         }
 
         void Start()
         {
-            BuildMesh();
+            diskMesh.Build(size, MaxDisks);
             meshRenderer.material = defaultMaterial;
-        }
-
-        void BuildMesh()
-        {
-            Mesh mesh = new Mesh();
-            
-            vertices.Clear();
-            triangles.Clear();
-
-            Triangulate();
-
-            mesh.vertices = vertices.ToArray();
-            mesh.triangles = triangles.ToArray();
-            mesh.RecalculateNormals();
-
-            meshFilter.mesh = mesh;
-        }
-
-        void Triangulate()
-        {
-            float alpha = Mathf.PI / numberOfSegments * 2;
-            float totalAlpha = 0;
-            float stepSize = MaxDisks > 1 ? (maxRadius - minRadius) / (MaxDisks - 1) : 0f;
-            float radius = minRadius + stepSize * (Size - 1);
-
-            for (int i = 0; i < numberOfSegments; i++)
-            {
-                Vector3 c1 = new Vector3(innerRadius * Mathf.Cos(totalAlpha), height,
-                    innerRadius * Mathf.Sin(totalAlpha));
-                Vector3 v1 = new Vector3(radius * Mathf.Cos(totalAlpha), height, radius * Mathf.Sin(totalAlpha));
-
-                Vector3 c2 = new Vector3(innerRadius * Mathf.Cos(totalAlpha), 0f, innerRadius * Mathf.Sin(totalAlpha));
-                Vector3 v2 = new Vector3(radius * Mathf.Cos(totalAlpha), 0f, radius * Mathf.Sin(totalAlpha));
-
-                totalAlpha += alpha;
-
-                Vector3 c3 = new Vector3(innerRadius * Mathf.Cos(totalAlpha), height,
-                    innerRadius * Mathf.Sin(totalAlpha));
-                Vector3 v3 = new Vector3(radius * Mathf.Cos(totalAlpha), height, radius * Mathf.Sin(totalAlpha));
-
-                Vector3 c4 = new Vector3(innerRadius * Mathf.Cos(totalAlpha), 0f, innerRadius * Mathf.Sin(totalAlpha));
-                Vector3 v4 = new Vector3(radius * Mathf.Cos(totalAlpha), 0f, radius * Mathf.Sin(totalAlpha));
-
-                AddQuad(c1, c3, c4, c2);
-                AddQuad(c1, v1, v3, c3);
-                AddQuad(c2, c4, v4, v2);
-                AddQuad(v1, v2, v4, v3);
-            }
-        }
-
-        void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
-        {
-            vertices.Add(v3);
-            vertices.Add(v2);
-            vertices.Add(v1);
-
-            int current = triangles.Count;
-            triangles.Add(current + 0);
-            triangles.Add(current + 1);
-            triangles.Add(current + 2);
-        }
-
-        void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
-        {
-            AddTriangle(v1, v2, v3);
-            AddTriangle(v1, v3, v4);
         }
     }
 }
---
+++ Assets/Things/Disk/DiskMesh.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace HanoiTowers
+{
+    [RequireComponent(typeof(MeshFilter))]
+    public class DiskMesh : MonoBehaviour
+    {
+        [SerializeField] float minRadius = 0.2f;
+        [SerializeField] float maxRadius = 1.2f;
+        [SerializeField] float innerRadius = 0.15f;
+        [SerializeField] int numberOfSegments = 128;
+        [SerializeField] float height = 0.2f;
+        
+        MeshFilter meshFilter;
+
+        readonly List<Vector3> vertices = new List<Vector3>();
+        readonly List<int> triangles = new List<int>();
+
+        public float Height => height;
+
+        void Awake()
+        {
+            meshFilter = GetComponent<MeshFilter>();
+        }
+
+        public void Build(int size, int maxDisks)
+        {
+            Mesh mesh = new Mesh();
+
+            vertices.Clear();
+            triangles.Clear();
+
+            Triangulate(size, maxDisks);
+
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.RecalculateNormals();
+
+            meshFilter.mesh = mesh;
+        }
+
+        void Triangulate(int size, int maxDisks)
+        {
+            float alpha = Mathf.PI / numberOfSegments * 2;
+            float totalAlpha = 0;
+            float stepSize = maxDisks > 1 ? (maxRadius - minRadius) / (maxDisks - 1) : 0f;
+            float radius = minRadius + stepSize * (size - 1);
+
+            for (int i = 0; i < numberOfSegments; i++)
+            {
+                Vector3 c1 = new Vector3(innerRadius * Mathf.Cos(totalAlpha), height,
+                    innerRadius * Mathf.Sin(totalAlpha));
+                Vector3 v1 = new Vector3(radius * Mathf.Cos(totalAlpha), height, radius * Mathf.Sin(totalAlpha));
+
+                Vector3 c2 = new Vector3(innerRadius * Mathf.Cos(totalAlpha), 0f, innerRadius * Mathf.Sin(totalAlpha));
+                Vector3 v2 = new Vector3(radius * Mathf.Cos(totalAlpha), 0f, radius * Mathf.Sin(totalAlpha));
+
+                totalAlpha += alpha;
+
+                Vector3 c3 = new Vector3(innerRadius * Mathf.Cos(totalAlpha), height,
+                    innerRadius * Mathf.Sin(totalAlpha));
+                Vector3 v3 = new Vector3(radius * Mathf.Cos(totalAlpha), height, radius * Mathf.Sin(totalAlpha));
+
+                Vector3 c4 = new Vector3(innerRadius * Mathf.Cos(totalAlpha), 0f, innerRadius * Mathf.Sin(totalAlpha));
+                Vector3 v4 = new Vector3(radius * Mathf.Cos(totalAlpha), 0f, radius * Mathf.Sin(totalAlpha));
+
+                AddQuad(c1, c3, c4, c2);
+                AddQuad(c1, v1, v3, c3);
+                AddQuad(c2, c4, v4, v2);
+                AddQuad(v1, v2, v4, v3);
+            }
+        }
+
+        void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+        {
+            vertices.Add(v3);
+            vertices.Add(v2);
+            vertices.Add(v1);
+
+            int current = triangles.Count;
+            triangles.Add(current + 0);
+            triangles.Add(current + 1);
+            triangles.Add(current + 2);
+        }
+
+        void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
+        {
+            AddTriangle(v1, v2, v3);
+            AddTriangle(v1, v3, v4);
+        }
+    }
+}
